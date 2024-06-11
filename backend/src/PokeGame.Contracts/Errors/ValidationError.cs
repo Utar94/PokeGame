77b9642c77026c6738acaa728cb8db3a024242b2
Francis Pion <francispion@hotmail.com>
2024-06-11 00:54:34 -0400

    Contracts.

---
+++ backend/src/PokeGame.Contracts/Errors/ValidationError.cs
@@ -0,0 +1,23 @@
+﻿using Logitar.Portal.Contracts.Errors;
+
+namespace PokeGame.Contracts.Errors;
+
+public record ValidationError : Error
+{
+  public List<PropertyError> Errors { get; set; }
+
+  public ValidationError() : this("Validation", "Validation failed.")
+  {
+  }
+
+  public ValidationError(string code, string message) : this(code, message, [])
+  {
+  }
+
+  public ValidationError(string code, string message, IEnumerable<ErrorData> data) : base(code, message, data)
+  {
+    Errors = [];
+  }
+
+  public void Add(PropertyError error) => Errors.Add(error);
+}
