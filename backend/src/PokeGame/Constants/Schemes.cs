f5a7bba266d5655a2f7dfa22403fac3b576f17d8
Francis Pion <francispion@hotmail.com>
2024-06-12 20:48:32 -0400

    Implemented Basic authentication & authorization.

---
+++ backend/src/PokeGame/Constants/Schemes.cs
@@ -0,0 +1,18 @@
+﻿namespace PokeGame.Constants;
+
+internal static class Schemes
+{
+  public const string Basic = nameof(Basic);
+
+  public static string[] GetEnabled(IConfiguration configuration)
+  {
+    List<string> schemes = new(capacity: 4);
+
+    if (configuration.GetValue<bool>("EnableBasicAuthentication"))
+    {
+      schemes.Add(Basic);
+    }
+
+    return [.. schemes];
+  }
+}
