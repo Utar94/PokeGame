99a25f3022c41e9b6290f84132a4a99be4f7f5ff
Francis Pion <francispion@hotmail.com>
2024-06-14 00:26:06 -0400

    Controller & Models.

---
+++ backend/src/PokeGame/Controllers/MoveController.cs
@@ -0,0 +1,64 @@
+﻿using Logitar.Portal.Contracts.Search;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using PokeGame.Application;
+using PokeGame.Application.Moves.Commands;
+using PokeGame.Application.Moves.Queries;
+using PokeGame.Constants;
+using PokeGame.Contracts.Moves;
+using PokeGame.Extensions;
+using PokeGame.Models.Moves;
+
+namespace PokeGame.Controllers;
+
+[ApiController]
+[Authorize(Policy = Policies.Gamemaster)]
+[Route("moves")]
+public class MoveController : ControllerBase
+{
+  private readonly IRequestPipeline _pipeline;
+
+  public MoveController(IRequestPipeline pipeline)
+  {
+    _pipeline = pipeline;
+  }
+
+  [HttpPost]
+  public async Task<ActionResult<Move>> CreateAsync([FromBody] CreateMovePayload payload, CancellationToken cancellationToken)
+  {
+    Move move = await _pipeline.ExecuteAsync(new CreateMoveCommand(payload), cancellationToken);
+    Uri location = HttpContext.BuildLocation("moves", new Dictionary<string, string>
+    {
+      ["id"] = move.Id.ToString()
+    });
+    return Created(location, move);
+  }
+
+  [HttpGet("{id}")]
+  public async Task<ActionResult<Move>> ReadAsync(Guid id, CancellationToken cancellationToken)
+  {
+    Move? move = await _pipeline.ExecuteAsync(new ReadMoveQuery(id, UniqueName: null), cancellationToken);
+    return move == null ? NotFound() : Ok(move);
+  }
+
+  [HttpGet("unique-name:{uniqueName}")]
+  public async Task<ActionResult<Move>> ReadAsync(string uniqueName, CancellationToken cancellationToken)
+  {
+    Move? move = await _pipeline.ExecuteAsync(new ReadMoveQuery(Id: null, uniqueName), cancellationToken);
+    return move == null ? NotFound() : Ok(move);
+  }
+
+  [HttpPut("{id}")]
+  public async Task<ActionResult<Move>> ReplaceAsync(Guid id, [FromBody] ReplaceMovePayload payload, long? version, CancellationToken cancellationToken = default)
+  {
+    Move? move = await _pipeline.ExecuteAsync(new ReplaceMoveCommand(id, payload, version), cancellationToken);
+    return move == null ? NotFound() : Ok(move);
+  }
+
+  [HttpGet]
+  public async Task<ActionResult<SearchResults<Move>>> SearchAsync([FromQuery] SearchMovesParameters parameters, CancellationToken cancellationToken)
+  {
+    SearchResults<Move> moves = await _pipeline.ExecuteAsync(new SearchMovesQuery(parameters.ToPayload()), cancellationToken);
+    return Ok(moves);
+  }
+}
---
+++ backend/src/PokeGame/Models/Moves/SearchMovesParameters.cs
@@ -0,0 +1,42 @@
+﻿using Logitar.Portal.Contracts.Search;
+using Microsoft.AspNetCore.Mvc;
+using PokeGame.Contracts;
+using PokeGame.Contracts.Moves;
+using PokeGame.Models.Search;
+
+namespace PokeGame.Models.Moves;
+
+public record SearchMovesParameters : SearchParameters
+{
+  [FromQuery(Name = "type")]
+  public PokemonType? Type { get; set; }
+
+  [FromQuery(Name = "category")]
+  public MoveCategory? Category { get; set; }
+
+  public SearchMovesPayload ToPayload()
+  {
+    SearchMovesPayload payload = new()
+    {
+      Type = Type,
+      Category = Category
+    };
+
+    FillPayload(payload);
+
+    List<SortOption>? sortOptions = ((SearchPayload)payload).Sort;
+    if (sortOptions != null)
+    {
+      payload.Sort = new List<MoveSortOption>(capacity: sortOptions.Count);
+      foreach (SortOption sort in sortOptions)
+      {
+        if (Enum.TryParse(sort.Field, out MoveSort field))
+        {
+          payload.Sort.Add(new MoveSortOption(field, sort.IsDescending));
+        }
+      }
+    }
+
+    return payload;
+  }
+}
