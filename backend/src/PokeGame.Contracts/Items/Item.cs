15d4efc1be0956e68acee896763bceb463dd4262
Francis Pion <francispion@hotmail.com>
2024-06-18 16:13:57 -0400

    Contracts.

---
+++ backend/src/PokeGame.Contracts/Items/Item.cs
@@ -0,0 +1,28 @@
+﻿using Logitar.Portal.Contracts;
+
+namespace PokeGame.Contracts.Items;
+
+public class Item : Aggregate
+{
+  public ItemCategory Category { get; set; }
+  public int? Price { get; set; }
+
+  public string UniqueName { get; set; }
+  public string? DisplayName { get; set; }
+  public string? Description { get; set; }
+  public string? Picture { get; set; }
+
+  public string? Reference { get; set; }
+  public string? Notes { get; set; }
+
+  public Item() : this(string.Empty)
+  {
+  }
+
+  public Item(string uniqueName)
+  {
+    UniqueName = uniqueName;
+  }
+
+  public override string ToString() => $"{DisplayName ?? UniqueName} | {base.ToString()}";
+}
---
+++ backend/src/PokeGame.Contracts/Items/ItemSortOption.cs
@@ -0,0 +1,20 @@
+﻿using Logitar.Portal.Contracts.Search;
+
+namespace PokeGame.Contracts.Items;
+
+public record ItemSortOption : SortOption
+{
+  public new ItemSort Field
+  {
+    get => Enum.Parse<ItemSort>(base.Field);
+    set => base.Field = value.ToString();
+  }
+
+  public ItemSortOption() : this(ItemSort.DisplayName)
+  {
+  }
+
+  public ItemSortOption(ItemSort field, bool isDescending = false) : base(field.ToString(), isDescending)
+  {
+  }
+}
