dae4b2cafbfb4730693fc47754e45b57ee29da75
Francis Pion <francispion@hotmail.com>
2024-06-18 11:59:04 -0400

    Contracts.

---
+++ backend/src/PokeGame.Contracts/Regions/Region.cs
@@ -0,0 +1,24 @@
+﻿using Logitar.Portal.Contracts;
+
+namespace PokeGame.Contracts.Regions;
+
+public class Region : Aggregate
+{
+  public string UniqueName { get; set; }
+  public string? DisplayName { get; set; }
+  public string? Description { get; set; }
+
+  public string? Reference { get; set; }
+  public string? Notes { get; set; }
+
+  public Region() : this(string.Empty)
+  {
+  }
+
+  public Region(string uniqueName)
+  {
+    UniqueName = uniqueName;
+  }
+
+  public override string ToString() => $"{DisplayName ?? UniqueName} | {base.ToString()}";
+}
---
+++ backend/src/PokeGame.Contracts/Regions/RegionSort.cs
@@ -0,0 +1,8 @@
+﻿namespace PokeGame.Contracts.Regions;
+
+public enum RegionSort
+{
+  DisplayName,
+  UniqueName,
+  UpdatedOn
+}
