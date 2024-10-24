c165784a08128576b3f09b66b01cb128fd23faab
Francis Pion <francispion@hotmail.com>
2024-10-24 14:45:01 -0400

    domain

---
+++ backend/src/PokeGame.Domain/Abilities/AbilityId.cs
@@ -0,0 +1,26 @@
+﻿using Logitar.EventSourcing;
+
+namespace PokeGame.Domain.Abilities;
+
+public readonly struct AbilityId
+{
+  public AggregateId AggregateId { get; }
+  public string Value => AggregateId.Value;
+
+  public AbilityId(Guid value) : this(new AggregateId(value))
+  {
+  }
+  public AbilityId(AggregateId aggregateId)
+  {
+    AggregateId = aggregateId;
+  }
+
+  public static AbilityId NewId() => new(AggregateId.NewId());
+
+  public static bool operator ==(AbilityId left, AbilityId right) => left.Equals(right);
+  public static bool operator !=(AbilityId left, AbilityId right) => !left.Equals(right);
+
+  public override bool Equals([NotNullWhen(true)] object? obj) => obj is AbilityId id && id.Value == Value;
+  public override int GetHashCode() => Value.GetHashCode();
+  public override string ToString() => Value;
+}
---
+++ backend/src/PokeGame.Domain/Description.cs
@@ -0,0 +1,27 @@
+﻿using FluentValidation;
+using PokeGame.Domain.Validators;
+
+namespace PokeGame.Domain;
+
+public record Description
+{
+  public string Value { get; }
+
+  public Description(string value)
+  {
+    Value = value.Trim();
+    new Validator().ValidateAndThrow(this);
+  }
+
+  public static Description? TryCreate(string? value) => string.IsNullOrWhiteSpace(value) ? null : new(value);
+
+  public override string ToString() => Value;
+
+  private class Validator : AbstractValidator<Description>
+  {
+    public Validator()
+    {
+      RuleFor(x => x.Value).Description();
+    }
+  }
+}
