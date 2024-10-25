ff6c3ad82036aacf3102109b1d873e3f0461ed85
Francis Pion <francispion@hotmail.com>
2024-10-24 21:13:50 -0400

    api

--- backend/src/PokeGame/Program.cs
+++ backend/src/PokeGame/Program.cs
@@ -1,17 +1,28 @@
-﻿namespace PokeGame;
+﻿using MediatR;
+using PokeGame.Infrastructure.Commands;
+
+namespace PokeGame;
 
 internal class Program
 {
-  public static void Main(string[] args)
+  public static async Task Main(string[] args)
   {
     WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+    IConfiguration configuration = builder.Configuration;
 
-    Startup startup = new(builder.Configuration);
+    Startup startup = new(configuration);
     startup.ConfigureServices(builder.Services);
 
     WebApplication application = builder.Build();
 
     startup.Configure(application);
+
+    if (configuration.GetValue<bool>("EnableMigrations"))
+    {
+      using IServiceScope scope = application.Services.CreateScope();
+      IPublisher publisher = scope.ServiceProvider.GetRequiredService<IPublisher>();
+      await publisher.Publish(new InitializeDatabaseCommand());
+    }
 
     application.Run();
   }
