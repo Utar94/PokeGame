82363c665c754975d4351dd19cb0e3c7fb6a3678
Francis Pion <francispion@hotmail.com>
2024-10-24 21:04:42 -0400

    application

---
+++ backend/src/PokeGame.Application/Caching/ICacheService.cs
@@ -0,0 +1,10 @@
+﻿using Logitar.EventSourcing;
+using Logitar.Portal.Contracts.Actors;
+
+namespace PokeGame.Application.Caching;
+
+public interface ICacheService
+{
+  Actor? GetActor(ActorId id);
+  void SetActor(Actor actor);
+}
