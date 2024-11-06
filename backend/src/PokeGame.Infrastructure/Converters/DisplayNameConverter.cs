62dfdb5a1c2047c942d8534de30028c8cc6a60e2
Francis Pion <francispion@hotmail.com>
2024-11-06 16:45:15 -0500

    infrastructure

---
+++ backend/src/PokeGame.Infrastructure/Converters/DisplayNameConverter.cs
@@ -0,0 +1,16 @@
+﻿using PokeGame.Domain;
+
+namespace PokeGame.Infrastructure.Converters;
+
+internal class DisplayNameConverter : JsonConverter<DisplayName>
+{
+  public override DisplayName? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+  {
+    return DisplayName.TryCreate(reader.GetString());
+  }
+
+  public override void Write(Utf8JsonWriter writer, DisplayName displayName, JsonSerializerOptions options)
+  {
+    writer.WriteStringValue(displayName.Value);
+  }
+}
